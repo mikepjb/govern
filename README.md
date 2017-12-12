8a9a24a21e2a26beb6c9fcea11de93a7fe609641
mikepjb <mikepjb@gmail.com>
2017-12-12 13:53:11 +0000

    Runs go test when given a .go file

---
+++ README.md
@@ -0,0 +1,8 @@
+# govern
+
+from Ancient Greek κυβερνάω (kubernáō, “I steer, drive, govern”)
+
+This is a tool that figures out what I intend to do when working with a file in
+vim. For example in ruby, I want to run rspec but in clojure test files I want
+to evaluate the current file and additionally run tests if clojure.test is
+required.
---
+++ govern.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+  "fmt"
+  "flag"
+  "os"
+  "os/exec"
+  "log"
+  "bytes"
+  "strings"
+)
+
+func runCommand(command string) {
+  args := strings.Fields(command)
+
+  var cmd *exec.Cmd
+
+  if len(args) < 2 {
+    cmd = exec.Command(args[0])
+  } else {
+    cmd = exec.Command(args[0], args[1:]...)
+  }
+  var out bytes.Buffer
+  cmd.Stdout = &out
+  err := cmd.Run()
+  if err != nil {
+    log.Fatal(err)
+  }
+  fmt.Print(out.String())
+}
+
+func main() {
+  filePtr := flag.String("file", "", "File to determine a task for")
+  flag.Parse()
+
+  if *filePtr == "" {
+    fmt.Println("no file supplied to govern, please use the -file flag")
+    os.Exit(0)
+  }
+
+  // TODO find the full filepath by combining current dir with parameter
+  filePath := *filePtr
+
+  if strings.HasSuffix(filePath, ".clj") {
+    fmt.Println("clojure file found, executing load-file")
+    // TODO inspect file for deftest OR clojure.test for more stable address.
+  } else if strings.HasSuffix(filePath, ".go") {
+    fmt.Println("go file found, executing go test")
+    runCommand("go test")
+  }
+
+}
