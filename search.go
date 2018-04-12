7dd4009956dc908f6dacc7145382c5cc2cfe328e
mikepjb <mikepjb@gmail.com>
2018-04-12 18:56:27 +0100

    collects file list filtering out .git files

--- search.go
+++ search.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 func FilesAvailable() int {
@@ -18,10 +21,23 @@ func GetBuffer() string {
 	return buffer
 }
 
+func FilesInTree(root string) []string {
+	var files []string
+
+	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if !strings.Contains(path, ".git") && !info.IsDir() {
+			files = append(files, path)
+		}
+		return nil
+	})
+
+	return files
+}
+
 // Controlling the terminal - need access to stdin/out
 // then we send vt100 commands etc there.
 
 // here we are making space for the fuzzy search interface
 func MakeRoom() {
-	fmt.Printf("\n>>>\n\n\n\n\n\n\n")
+	fmt.Printf(">>>\n\n\n\n\n\n\n\n\n\n\n")
 }
--- search_test.go
+++ search_test.go
@@ -4,9 +4,30 @@ import (
 	"testing"
 )
 
+func sliceContains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
 func TestTheNumberOfFilesOfProject(t *testing.T) {
 	if FilesAvailable() != 5 {
 		t.Errorf("Number of files is %v", FilesAvailable())
+	}
+}
+
+func TestListFilesInTree(t *testing.T) {
+	var fileList = FilesInTree(".")
+
+	if !sliceContains(fileList, "govern.go") {
+		t.Errorf("file list does not contain govern.go")
+	}
+
+	if !sliceContains(fileList, "search_test.go") {
+		t.Errorf("file list does not contain search_test.go")
 	}
 }
 
